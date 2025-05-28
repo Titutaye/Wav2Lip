6730bbed0e585d59f182db086e4e69b6ff2da419
TiTu Taye <titu.cse.21@nitap.ac.in>
2025-05-28 23:39:15 +0530

    Update calculate_scores_LRS.py

--- evaluation/scores_LSE/calculate_scores_LRS.py
+++ evaluation/scores_LSE/calculate_scores_LRS.py
@@ -46,7 +46,7 @@ for videofile_idx in prog_bar:
 	prog_bar.set_description('Avg Confidence: {}, Avg Minimum Dist: {}'.format(round(avg_confidence / (videofile_idx + 1), 3), round(avg_min_distance / (videofile_idx + 1), 3)))
 	prog_bar.refresh()
 
-#print("The Evaluated Result of the Project "Making Image Speak By Using Wav2lip")
+print("The Evaluated Result of the Project "Making Image Speak By Using Wav2lip\n")
 print ('Average Confidence: {}'.format(avg_confidence/len(all_videos)))
 print ('Average Minimum Distance: {}'.format(avg_min_distance/len(all_videos)))
 
